d7080628fe4aa0da6a35fdcd7083bc9ea0d67c3e
github username <github email address>
2017-09-18 15:24:40 -0400

    create video list and video items

---
+++ src/components/video_list.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import VideListItem from './video_list_item'
+
+const VideoList = (props) => {
+  props.videos.map((video) => {
+    return <VideoListItem video={video} />
+  });
+
+
+  return (
+    <ul className='col-md-4 list-group'>
+      {videoItems}
+    </ul>
+  )
+}
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAz0JEJqpfEAgudUa8o1WtE4Mi-pz4LaxI';
 
 
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = { videos: []}
 
-// Create a new component. this component should produce some HTML
-const App = () => {
-  return (
+    YTSearch({key: API_Key, term: 'surfboards'}, (videos) =>{
+      this.setState({ videos })
+    })
+  }
+
+  render(){
     <div>
       <SearchBar />
+      <VideoList vidoes={this.state.videos} />
     </div>;
-  )
+  }
 }
 
-
-// We have to specifically say that this component needs to be generated into the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
