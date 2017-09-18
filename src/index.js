94d3d3162c34a4a13d33801c83a193c5014f3f53
github username <github email address>
2017-09-18 17:41:59 -0400

    video lists and video list items

--- src/index.js
+++ src/index.js
@@ -1,28 +1,39 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAz0JEJqpfEAgudUa8o1WtE4Mi-pz4LaxI';
 
 
 class App extends Component {
   constructor(props){
-    super(props)
-    this.state = { videos: []}
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
 
-    YTSearch({key: API_Key, term: 'surfboards'}, (videos) =>{
-      this.setState({ videos })
+    YTSearch({key: API_Key, term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       })
     })
   }
 
   render(){
+    return (
     <div>
       <SearchBar />
-      <VideoList vidoes={this.state.videos} />
-    </div>;
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList
+      onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+      videos={this.state.videos} />
+    </div>
+    )
   }
 }
 
