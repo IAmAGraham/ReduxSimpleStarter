1acdfbc0e1efcb2345e326ef162ed4027ff980e3
github username <github email address>
2017-09-18 12:30:48 -0400

    create component files and index.js file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. this component should produce some HTML
+const App = () => {
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// We have to specifically say that this component needs to be generated into the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
